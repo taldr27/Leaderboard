5a87a8e5f8286be8c3b5e9a66e56fd583c685270
taldr27 <diegogarciaq98@gmail.com>
2022-10-19 11:12:35 -0500

    created new game

--- src/modules/add.js
+++ src/modules/add.js
@@ -14,11 +14,25 @@ const add = (e) => {
     section.insertAdjacentElement('afterend', message);
     setTimeout(() => { message.remove(); }, 2000);
   } else {
-    const score = new Scores(name, scoreV);
+    // const score = new Scores(name, scoreV);
 
-    Interface.addScoreToList(score);
-    Store.addScore(score);
+    // Interface.addScoreToList(score);
+    // Store.addScore(score);
+    const data = { user: name, score: scoreV }; //gameID: ERLioRAyRDENKi33Awku;
+
+    fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/ERLioRAyRDENKi33Awku/scores/', {
+      method: 'POST', // or 'PUT'
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Success:', data);
+      });
   }
+
 };
 
 export default add;
