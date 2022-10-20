7f860eec79bce9752b077a58a0f5ea20e17fe306
taldr27 <diegogarciaq98@gmail.com>
2022-10-20 11:55:14 -0500

    Added styles for the leaderboard

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import add from './modules/add.js';
 import refresh from './modules/refresh.js';
-import './style.css';
+import './index.css';
 
 document.querySelector('#score-form').addEventListener('submit', add);
 document.querySelector('#refreshBtn').addEventListener('click', refresh);
+window.onload = refresh();
+
+const data = { name: 'game' };
+
+fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/', {
+  method: 'POST',
+  body: JSON.stringify(data),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+})
+  .then((response) => response.json())
+  .then((json) => console.log(json));
